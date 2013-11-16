4759cb74c3bd28596af577ebdda824f0d7d9a946
indika pathi <indika@mail.usf.edu>
2013-11-15 20:23:19 -0500

    allows to specify control frame on the command line
    plus bunch of other stufff

--- test2/application.cpp
+++ test2/application.cpp
@@ -27,7 +27,44 @@ int main()
 
 	if(wmraArm.initialize()){
 		//cout << "Controller intialized in main" << endl;
+      WMRA::Pose readyPose = wmraArm.getPose();
+      int cordframe;
+      double temp;
 		while(!endFlag){
+         cout << "Current Position is :" << endl;
+         /*cin >> temp;
+         if(temp == 0) endFlag = true;*/
+         WMRA::Pose pose = wmraArm.getPose();
+         cout << "x = " << pose.x << ", y = " << pose.y << ", z = " << pose.z ; 
+         cout << " ,yaw= " << radToDeg(pose.yaw) << " ,pitch= " << radToDeg(pose.pitch) << " ,roll= " << radToDeg(pose.roll) <<endl;
+
+         //WMRA::Pose destpose;
+         //destpose = pose;
+         //destpose.y += 400;
+         //destpose.z += -200;
+         //wmraArm.autonomous2(destpose, WMRA::ARM_FRAME_ABS);
+         //Sleep(9000);
+         //destpose = wmraArm.getPose();
+         //destpose.yaw += degToRad(10);
+         //wmraArm.autonomous2(destpose, WMRA::ARM_FRAME_ABS);
+         //Sleep(5000);
+
+         //WMRA::Pose pose2;
+         //pose2.z = 150;
+         //pose2.pitch = -30;
+         //wmraArm.autonomous2(pose2, WMRA::GRIPPER_FRAME_REL);
+         //Sleep(6000);
+
+
+         //WMRA::Pose pose3 = pose;
+         //pose3.x += -100;
+         //pose3.yaw = -180;
+         //wmraArm.autonomous2(pose3, WMRA::ARM_FRAME_ABS); //goto ready
+
+
+
+         
+
 			cout << "\n\nSelect a destination" << endl;
 			cout << "dx = ";
 			cin >> dest.x;
@@ -36,12 +73,18 @@ int main()
 			cout << "dz = ";
 			cin >> dest.z;
 			cout << "Pitch = ";
-			cin >> dest.pitch;
+			cin >> temp;
+         dest.pitch = degToRad(temp);;
 			cout << "Roll = ";
-			cin >> dest.roll;
+			cin >> temp;
+         dest.roll = degToRad(temp);
 			cout << "Yaw = ";
-			cin >> dest.yaw;
+			cin >> temp;
+         dest.yaw = degToRad(temp);
 			cout << endl;
+         cout << "cord frame : 1=ABS, 2=REl, 3=Gripper" ; 
+         cin >> cordframe;
+         
 
 			if(dest.yaw == 555)
 				wmraArm.toReady();
@@ -52,9 +95,23 @@ int main()
 				wmraArm.closeDebug();
 				endFlag = true; // Will break out of loop
 			}
-			else
-				wmraArm.autonomous(dest,WMRA::ARM_FRAME); // Moves arm
+         else{
+
+            if(cordframe== 1){
+            wmraArm.autonomous2(dest, WMRA::ARM_FRAME_ABS); // Moves arm
+            }
+            if(cordframe== 2){
+               wmraArm.autonomous2(dest, WMRA::ARM_FRAME_REL); // Moves arm
+            }
+            if(cordframe== 3){
+               wmraArm.autonomous2(dest, WMRA::GRIPPER_FRAME_REL); // Moves arm
+            }
+         }
 		}
+
+      wmraArm.autonomous2(readyPose, WMRA::ARM_FRAME_ABS);
+
+
 	}
 	else
 		cout << "Controller initalizaition failed in main" << endl;
