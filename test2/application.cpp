2ac31440a5a81c26cf767f1f1f4036e5a1c0c47d
indika pathi <indika@mail.usf.edu>
2013-11-18 11:33:05 -0500

    added functionality to go to ready

--- test2/application.cpp
+++ test2/application.cpp
@@ -32,8 +32,7 @@ int main()
       double temp;
 		while(!endFlag){
          cout << "Current Position is :" << endl;
-         /*cin >> temp;
-         if(temp == 0) endFlag = true;*/
+
          WMRA::Pose pose = wmraArm.getPose();
          cout << "x = " << pose.x << ", y = " << pose.y << ", z = " << pose.z ; 
          cout << " ,yaw= " << radToDeg(pose.yaw) << " ,pitch= " << radToDeg(pose.pitch) << " ,roll= " << radToDeg(pose.roll) <<endl;
@@ -48,14 +47,11 @@ int main()
          //destpose.yaw += degToRad(10);
          //wmraArm.autonomous2(destpose, WMRA::ARM_FRAME_ABS);
          //Sleep(5000);
-
          //WMRA::Pose pose2;
          //pose2.z = 150;
          //pose2.pitch = -30;
          //wmraArm.autonomous2(pose2, WMRA::GRIPPER_FRAME_REL);
          //Sleep(6000);
-
-
          //WMRA::Pose pose3 = pose;
          //pose3.x += -100;
          //pose3.yaw = -180;
@@ -107,10 +103,18 @@ int main()
                wmraArm.autonomous2(dest, WMRA::GRIPPER_FRAME_REL); // Moves arm
             }
          }
+         Sleep(10000);
+
+         cout << "Select an option (0=Stop 1=Continue 2=Go to Ready) : " << endl;
+         int option;
+         cin >> option;
+         if(option == 2){
+            wmraArm.autonomous2(readyPose, WMRA::ARM_FRAME_ABS);
+         }
+         else if(option == 0){
+            endFlag = false;
+         }
 		}
-
-      wmraArm.autonomous2(readyPose, WMRA::ARM_FRAME_ABS);
-
 
 	}
 	else
