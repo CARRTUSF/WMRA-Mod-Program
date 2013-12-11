238739c2bda8e3cdb8e66a648e91f57c05e2c443
indika pathi <indika@mail.usf.edu>
2013-12-11 14:03:18 -0500

    UDP comm with BCI program to control arm

--- test2/application.cpp
+++ test2/application.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <vector>
 #include <Afxwin.h>
+#include "SockStream.h"
 #include "matrix.h" 
 #include "MotorController.h"
 #include "Arm.h"
@@ -18,6 +19,26 @@ char *tempChar; //temporary char pointer passed to thread
 Arm wmraArm;
 
 
+sending_udpsocket socket1( "localhost:6001" );
+sockstream output_sock( socket1 );
+receiving_udpsocket socket2( "localhost:6000" );
+sockstream read_sock( socket2 );
+
+int wait4Command(){
+   /*  Sleep(5000);
+   return;*/
+   char temp_buf[200]; 
+   int object_id = 0;
+   string temp_str_buf;
+   getline( read_sock, temp_str_buf );
+   while( temp_str_buf.find("OBJECT")== string::npos ){
+      getline( read_sock, temp_str_buf );
+      Sleep(20);
+   }
+   sscanf(temp_str_buf.c_str() , "%s %d",temp_buf, &object_id);
+   return object_id;
+}
+
 WMRA::Pose getUserDest(){
    WMRA::Pose dest;
    double temp;
@@ -30,7 +51,7 @@ WMRA::Pose getUserDest(){
    cin >> dest.z;
    cout << "Pitch = ";
    cin >> temp;
-   dest.pitch = degToRad(temp);;
+   dest.pitch = degToRad(temp);
    cout << "Roll = ";
    cin >> temp;
    dest.roll = degToRad(temp);
@@ -42,6 +63,70 @@ WMRA::Pose getUserDest(){
    return dest;    
 }
 
+
+
+void BCI_motion(){
+
+   cout << " started BCI loop" << endl;
+
+   WMRA::Pose dest;
+   WMRA::Pose readyPose = wmraArm.getPose();
+   int object_id =3;
+   while(true){
+      //object_id = wait4Command(); // wait for object id from VisualBCI
+      if(true/*object_id ==3*/){
+         //goto object
+         dest.x = 650;
+         dest.y = -100;
+         dest.z = 475;
+         dest.roll = 0;
+         dest.pitch = 0;
+         dest.yaw = degToRad(10);
+
+         wmraArm.autonomous2(dest, WMRA::ARM_FRAME_PILOT_MODE); // Moves arm
+         //Sleep(8000);
+
+         //bringback to user
+
+ /*        dest.x = 850;
+         dest.y = -100;
+         dest.z = 450;
+         dest.roll = degToRad(0);
+         dest.pitch = degToRad(0);
+         dest.yaw = degToRad(10);*/
+
+         dest.clear();
+         dest.z=100;
+
+
+
+         wmraArm.autonomous2(dest, WMRA::GRIPPER_FRAME_REL); // Moves arm
+         wmraArm.closeGripper();
+        
+
+
+
+         //goto user
+         dest.x = 0;
+         dest.y = -250;
+         dest.z = 480;
+         dest.roll = degToRad(0);
+         dest.pitch = degToRad(0);
+         dest.yaw = degToRad(-150);
+
+         wmraArm.autonomous2(dest, WMRA::ARM_FRAME_PILOT_MODE); // Moves arm
+         Sleep(1000);
+         wmraArm.openGripper();
+
+         //got to ready
+
+         wmraArm.autonomous2(readyPose, WMRA::ARM_FRAME_PILOT_MODE);
+         Sleep(8000);
+
+         output_sock << "DONE" << endl;
+      }
+   }
+}
 
 
 int main()
@@ -56,6 +141,8 @@ int main()
       double temp;
       int option;
 
+      BCI_motion();
+
       while(!endFlag){
          cout << "Current Position is :" << endl;
          WMRA::Pose pose = wmraArm.getPose();
@@ -64,7 +151,7 @@ int main()
 
          cout << "Select an option (0 = Exit  1 = Continue 2 = Go to Ready) : "; 
          cin >> option;
-        
+
          if(option == 1){
             WMRA::Pose dest = getUserDest();            
             cout << "Which cordinate frame? 1=ABS, 2=REL, 3=Gripper 7=skip: " ; 
@@ -90,7 +177,7 @@ int main()
             Sleep(10000); // wait for motion end
          }
          else if(option == 2){
-            wmraArm.autonomous2(readyPose, WMRA::ARM_FRAME_ABS);
+            wmraArm.autonomous2(readyPose, WMRA::ARM_FRAME_PILOT_MODE);
             Sleep(10000);
          }
          else if(option == 0){
@@ -101,15 +188,15 @@ int main()
       cout << "About to exit program. WOuld you like to go to ready position? 1=Yes 0=No : " ;
       cin >> option;
       if(option ==1){
-         wmraArm.autonomous2(readyPose, WMRA::ARM_FRAME_ABS);
+         wmraArm.autonomous2(readyPose, WMRA::ARM_FRAME_PILOT_MODE);
          Sleep(10000);
       }
-     
+
    }
    else{
       cout << "Controller initalizaition failed in main" << endl;
    }
 
-    cout << "Exiting Program..... Press any key to close this window." << endl;
-    cin.get();
+   cout << "Exiting Program..... Press any key to close this window." << endl;
+   cin.get();
 }
